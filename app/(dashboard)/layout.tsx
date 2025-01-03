1419b6cdef4223484f7711f5d7058f9804bce1ed
code <kabir.naved.khan@gmail.com>
2025-01-03 23:19:29 +0530

    first commit

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,14 @@
+
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+    <div>test1</div>
+    {children}
+    </>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <>
+      <h1 className= 'text-center p-2 bg-red-500'>Hi</h1>
+    </>
+  );
+}
