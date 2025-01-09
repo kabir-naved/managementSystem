72bbc209af8008dcabe69aeef265efea4ce3d6b9
code <kabir.naved.khan@gmail.com>
2025-01-09 22:42:01 +0530

    ui is created

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+const LoginPage = () => {
+  return (
+    <div className=''>LoginPage</div>
+  )
+}
 
-const Login = () => {
-  return <div>Login</div>;
-};
-
-export default Login;
+export default LoginPage
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["images.pexels.com"], // Add allowed image domains here
+  },
 };
 
 export default nextConfig;
